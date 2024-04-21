b6eaa7f92cd8d569bdb3d08485a141f8a682fb81
nikola6669 <77353945+nikext@users.noreply.github.com>
2024-04-21 20:46:28 +0200

    implement clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
+// "use client";
+
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div className="w-48 p-4" key={image.id}>
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div className="w-48 p-4" key={image.id}>
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in to continue
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
