6d31ca0176747e3a976c9a854d930a859efe510c
nikola6669 <77353945+nikext@users.noreply.github.com>
2024-04-22 19:14:59 +0200

    implement image upload

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utilsl/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
       <div>
+        <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={() => {
+            router.refresh();
+          }}
+        />
         <SignedOut>
           <SignInButton />
         </SignedOut>
